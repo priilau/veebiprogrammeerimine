62feae9eb075199035fe07f1cb21cf60e8f7f018
Priit Laupa <priilau@users.noreply.github.com>
2018-09-11 15:21:39 +0300

    Veebilehele lisatud php elemendid

--- index.php
+++ index.php
@@ -1,18 +1,50 @@
+<?php
+  //echo "See on minu esimene php!"; //lisamärkus
+  $firstName = "Priit";
+  $lastName = "Laupa";
+  $dateToday = date("d.m.Y");
+  $hourNow = date("G");
+  $partOfDay = "";
+  if ($hourNow >= 7 and $hourNow < 8){
+	  $partOfDay = "varajane Hommik.";
+  }
+  if ($hourNow >= 8 and $hourNow < 16){
+	  $partOfDay = "kooliaeg.";
+  }
+  if ($hourNow >= 16 and $hourNow <= 23){
+	  $partOfDay = "vabaaeg.";
+  }
+  if ($hourNow > 23 and $hourNow <= 7){
+	  $partOfDay = "magamise aeg.";
+  }
+  
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-	<title>Priit Laupa, õppetöö</title>
+	<title>
+	<?php
+	  echo $firstName; 
+	  echo " ";
+	  echo $lastName;
+	?>
+	, õppetöö</title>
   </head>
-  <body style="background-color:#FAEED2;">
-    <h1>Priit Laupa</h1>
+  <body>
+    <h1>
+	<?php
+	  echo $firstName ." " .$lastName;
+	?>
+	</h1>
 	<p>See leht on valminud <a href="https://www.tlu.ee">TLÜ</a> õppetöö raames ning ei oma mingisugust mõtestatud või muul moel väärtusliku sisu.</p>
-	<p style="color:#FF0000;">Klikka pildil</p>
+	<p>Klikka pildil</p>
 	
 	
-	<a href="https://upload.wikimedia.org/wikipedia/commons/e/eb/P._Kutser.jpg" target="_blank"><img src="../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_2.jpg" alt="TLÜ Terra õppehoone"></a>
+	<a href="https://upload.wikimedia.org/wikipedia/commons/e/eb/P._Kutser.jpg" target="_blank"><img src="../../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_2.jpg" alt="TLÜ Terra õppehoone"></a>
 	
-	<p>Minu <a href="../~jaagala" target="_blank">sõber</a> teeb ka veebi</p>
+	<p>Minu <a href="../../~jaagala" target="_blank">sõber</a> teeb ka veebi</p>
 	
 	
 	
@@ -24,6 +56,10 @@
 	<p>Ma ei valeta, võid rahulikult brauseri kinni panna.</p>
 	
 	<p> :( :( :( </p>
+	<?php
+	  echo "<p>Tänane kuupäev on: " .$dateToday .".</p> \n";
+	  echo "<p>Lehe avamise hetkel oli kell " .date("H:i") . " ning hetkel on " .$partOfDay ."</p> \n";
+	?>
 	
   </body>
 
